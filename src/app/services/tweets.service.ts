246e4b89707de57a88863cec47ff4a9c9de4bd4c
CojocariuAlexandru <53466892+CojocariuAlexandru@users.noreply.github.com>
2020-05-02 17:09:46 +0300

    updates design specification

--- src/app/services/tweets.service.ts
+++ src/app/services/tweets.service.ts
@@ -5,27 +5,32 @@ export class TweetsService {
   constructor(){}
   getTweets(){
     return [
-      {"name":"Alex", "message":"hai ca vom reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi", "sentiment":"positive"},
-      {"name":"Alex", "message":"hai ca vom reusi", "sentiment":"positive"},
-      {"name":"Alex", "message":"hai ca vom reusi  reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi", "sentiment":"positive"},
-      {"name":"Alex", "message":"hai ca vom reusi", "sentiment":"positive"},
-      {"name":"Alex", "message":"hai ca vom reusi", "sentiment":"positive"},
-      {"name":"Alex", "message":"hai ca vom reusi", "sentiment":"positive"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
-      {"name":"Ramona", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Alex", "tag":"mikMLG42069", "message":"hai ca vom reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi", "sentiment":"positive"},
+      {"name":"Alex", "tag":"mikMLG42069","message":"hai ca vom reusi", "sentiment":"positive"},
+      {"name":"Alex", "tag":"mikMLG42069","message":"hai ca vom reusi  reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi reusi", "sentiment":"positive"},
+      {"name":"Alex", "tag":"mikMLG42069","message":"hai ca vom reusi", "sentiment":"positive"},
+      {"name":"Alex", "tag":"mikMLG42069","message":"hai ca vom reusi", "sentiment":"positive"},
+      {"name":"Alex", "tag":"mikMLG42069","message":"hai ca vom reusi", "sentiment":"positive"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona","tag":"RAMOramo", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona","tag":"RAMOramo", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona","tag":"RAMOramo", "message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
+      {"name":"Ramona", "tag":"RAMOramo","message":"ce vrea e facultatea offf", "sentiment":"negative"},
 
 
-      {"name":"Mihai", "message":"ce?", "sentiment":"neutral"}
+      {"name":"Mihai", "tag":"master-BOWLER","message":"ce?", "sentiment":"neutral"},
+      {"name":"Mihai", "tag":"master-BOWLER","message":"ce?", "sentiment":"neutral"},
+      {"name":"Mihai", "tag":"master-BOWLER","message":"ce?", "sentiment":"neutral"},
+      {"name":"Mihai", "tag":"master-BOWLER","message":"ce?", "sentiment":"neutral"},
+      {"name":"Mihai", "tag":"master-BOWLER","message":"ce?", "sentiment":"neutral"}
+
     ];
   }
 }
