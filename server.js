878c620857661739c49908a000862a2821cda267
MasterBowler <40718703+MasterBowler@users.noreply.github.com>
2020-05-10 19:37:57 +0300

    Initial Heroku commit

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/tweets-classifier-spa'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tweets-classifier-spa/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
