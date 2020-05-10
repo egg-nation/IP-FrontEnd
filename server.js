f735a9943cebeb38f716324fd87dd98f926cd8f7
MasterBowler <40718703+MasterBowler@users.noreply.github.com>
2020-05-10 18:55:55 +0300

    Initial Heroku commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -9,4 +10,22 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+app.listen(process.env.PORT || 8080);
+*/
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
